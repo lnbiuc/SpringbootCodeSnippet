bd5ae2e8e0b2a4c13194faf6c40e8e57555542d0
ayanamirei <violetzzs@proton.me>
2022-10-06 20:05:31 +0800

    :tada:

---
+++ src/main/java/top/lnbiuc/use/SpringbootAllUseApplication.java
@@ -0,0 +1,15 @@
+package top.lnbiuc.use;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAllUseApplication
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringbootAllUseApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/top/lnbiuc/use/SpringbootAllUseApplicationTests.java
@@ -0,0 +1,15 @@
+package top.lnbiuc.use;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAllUseApplicationTests
+{
+    
+    @Test
+    void contextLoads()
+    {
+    }
+    
+}
